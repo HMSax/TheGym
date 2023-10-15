6839be3e7ec76f552d0387258f0056047bd83371
Martin Sax <h.martin.sax@gmail.com>
2023-10-16 00:21:53 +0200

    first completed test

--- src/TheGym.java
+++ src/TheGym.java
@@ -1,11 +1,57 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-public class TheGym
-{
-    public TheGym(){
+public class TheGym {
+    Path fromCustomersFilePath = Paths.get("src/customers.txt");
+    Path toPTFilePath = Paths.get("src/ptinfo.txt");
+    List<Customer> customerList = new ArrayList<>();
+
+    public TheGym() {
 
     }
-    public void mainProgram(){
 
+    public List<Customer> addToCustomerList(Scanner fileScanner) {
+        String firstLine;
+        String secondLine = "";
+        List<Customer> cList = new ArrayList<>();
+        String[] customerInfoFirstLine = new String[2];
+        while (fileScanner.hasNext()) {
+            firstLine = fileScanner.nextLine();
+            customerInfoFirstLine = firstLine.split(",");
+            if (fileScanner.hasNext()) {
+                secondLine = fileScanner.nextLine();
+            }
+            Customer customer = new Customer(customerInfoFirstLine[0], customerInfoFirstLine[1], secondLine);
+            cList.add(customer);
+        }
+        return cList;
+
+    }
+
+    public void mainProgram() {
+        try (Scanner fileScanner = new Scanner(fromCustomersFilePath);
+             PrintWriter printToFile = new PrintWriter(Files.newBufferedWriter(toPTFilePath))) {
+            customerList = addToCustomerList(fileScanner);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+            e.printStackTrace();
+            System.exit(0);
+        } catch (IOException e) {
+            System.out.println("Unable to write to file.");
+            e.printStackTrace();
+            System.exit(0);
+        } catch (Exception e) {
+            System.out.println("Something went wrong.");
+            e.printStackTrace();
+            System.exit(0);
+        }
     }
 
     public static void main(String[] args) {
--- test/TheGymTest.java
+++ test/TheGymTest.java
@@ -1,12 +1,45 @@
 import org.junit.jupiter.api.Test;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TheGymTest {
+    Path fromCustomersTestFilePath = Paths.get("test/customerTest.txt");
+    Path toPTinfoTestFilePath = Paths.get("test/ptinfoTest.txt");
 List<Customer> testList = new ArrayList<>();
+TheGym testGym = new TheGym();
     @Test
 
+public void addToCustomerListTest(){
+        try (Scanner fileScanner = new Scanner(fromCustomersTestFilePath)) {
+                testList = testGym.addToCustomerList(fileScanner);
+            assertTrue(testList.size() == 3);
+            assertFalse(testList.size() ==2);
+
+        }    catch (FileNotFoundException e){
+            System.out.println("File not found.");
+            e.printStackTrace();
+            System.exit(0);
+        }
+        catch (IOException e) {
+            System.out.println("Unable to write to file.");
+            e.printStackTrace();
+            System.exit(0);
+        }
+        catch (Exception e){
+            System.out.println("Something went wrong.");
+            e.printStackTrace();
+            System.exit(0);
+        }
+
+    }
 }
