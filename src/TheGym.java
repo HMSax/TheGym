461e10cd14a9a143aa4280127916c5ba310bcd0d
Martin Sax <h.martin.sax@gmail.com>
2023-10-16 18:26:09 +0200

    Revised code structure. Added comments.

--- src/TheGym.java
+++ src/TheGym.java
@@ -19,47 +19,7 @@ public class TheGym {
 
     }
 
-    public boolean currentMemberCheck(Customer customer) {
-        LocalDate membershipDate = LocalDate.parse(customer.getDateOfMembership());
-        if (membershipDate.isAfter(LocalDate.now().minusYears(1)) ||
-                membershipDate.isEqual(LocalDate.now().minusYears(1))) {
-            return true;
-        } else return false;
-    }
-
-    public boolean findCustomerInList(String userInput, List<Customer> cList) {
-        boolean isCustomer = false;
-        for (Customer c : cList) {
-            if (c.getName().equalsIgnoreCase(userInput)) isCustomer = true;
-            else if (c.getPersonalNumber().equals(userInput)) isCustomer = true;
-        }
-        return isCustomer;
-    }
-
-    public Customer getCustomerFromList(String userInput, List<Customer> cList) {
-        for (Customer c : cList) {
-            if (c.getName().equalsIgnoreCase(userInput) ||
-                    c.getPersonalNumber().equals(userInput)) return c;
-        }
-        return null;
-    }
-
-    public void printToPTInfo(Customer customer, Path outFilePath) {
-
-        try (PrintWriter printToFile = new PrintWriter(Files.newBufferedWriter(
-                outFilePath, StandardOpenOption.APPEND))) {
-            if (!Files.exists(outFilePath)) {
-                Files.createFile(outFilePath);
-            }
-            printToFile.write(customer.getPersonalNumber() + " " + customer.getName()
-                    + ", tränade " + LocalDate.now() + "\n");
-        } catch (IOException e) {
-            System.out.println("Unable to create file.");
-            e.printStackTrace();
-            System.exit(0);
-        }
-    }
-
+    // Läser från filen (customers.txt) och skapar upp samt returnerar en Customer-lista.
     public List<Customer> addToCustomerList(Scanner fileScanner) {
         String firstLine;
         String secondLine = "";
@@ -77,35 +37,85 @@ public class TheGym {
         return cList;
     }
 
+    //itererar över listan och returnerar true om namn eller personnummer matchar Customer-objekt i listan.
+    public boolean findCustomerInList(String userInput, List<Customer> cList) {
+        boolean isCustomer = false;
+        for (Customer c : cList) {
+            if (c.getName().equalsIgnoreCase(userInput)) isCustomer = true;
+            else if (c.getPersonalNumber().equals(userInput)) isCustomer = true;
+        }
+        return isCustomer;
+    }
+
+    //returnerar aktuell Customer från listan.
+    public Customer getCustomerFromList(String userInput, List<Customer> cList) {
+        for (Customer c : cList) {
+            if (c.getName().equalsIgnoreCase(userInput) ||
+                    c.getPersonalNumber().equals(userInput)) return c;
+        }
+        return null;
+    }
+
+    //returnerar true om aktuell Customer har aktivt medlemsskap.
+    public boolean currentMemberCheck(Customer customer) {
+        LocalDate membershipDate = LocalDate.parse(customer.getDateOfMembership());
+        if (membershipDate.isAfter(LocalDate.now().minusYears(1)) ||
+                membershipDate.isEqual(LocalDate.now().minusYears(1))) {
+            return true;
+        } else return false;
+    }
+
+    //skriver aktuell Customer och träningsdatum (dagens datum) till PT-filen.
+    public void printToPTInfo(Customer customer, Path outFilePath) {
+
+        try (PrintWriter printToFile = new PrintWriter(Files.newBufferedWriter(
+                outFilePath, StandardOpenOption.APPEND))) {
+            if (!Files.exists(outFilePath)) {
+                Files.createFile(outFilePath);
+            }
+            printToFile.write(customer.getPersonalNumber() + " " + customer.getName()
+                    + ", tränade " + LocalDate.now() + "\n");
+        } catch (IOException e) {
+            System.out.println("Unable to create file.");
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+
+    //huvudprogrammet innehållande det användaren ser.
     public void mainProgram() {
-        boolean isInList;
-        boolean isActivemember;
         String userInputString;
         Customer currentCustomer;
+        boolean programLoop = true;
 
         System.out.println("Welcome to Best Gym Ever!");
+
         try (Scanner fileScanner = new Scanner(fromCustomersFilePath); Scanner userScan = new Scanner(System.in)) {
             customerList = addToCustomerList(fileScanner);
-            System.out.println("Skriv namn eller personnummer:");
-            userInputString = userScan.nextLine().trim();
-            isInList = findCustomerInList(userInputString, customerList);
-            if (!isInList) {
-                System.out.println("Personen har aldrig varit medlem på Best Gym Ever.");
-                System.exit(0);
-            }
-            currentCustomer = getCustomerFromList(userInputString, customerList);
-            isActivemember = currentMemberCheck(currentCustomer);
-            if (!isActivemember) {
-                System.out.println("Personen har inte ett aktivt medlemskap. \n" +
-                        currentCustomer.getName() + " blev medlem "
-                        + currentCustomer.getDateOfMembership());
-                System.exit(0);
-            } else {
-                System.out.println(currentCustomer.getName() + " med personnummer " +
-                        currentCustomer.getPersonalNumber() + " är aktiv medlem på Best Gym Ever.");
-                printToPTInfo(currentCustomer, toPTFilePath);
-            }
+            while (programLoop) {
+                System.out.println("Skriv namn eller personnummer:");
+                userInputString = userScan.nextLine().trim();
 
+                if (!findCustomerInList(userInputString, customerList)) {
+                    System.out.println("Personen har aldrig varit medlem på Best Gym Ever.");
+                } else {
+                    currentCustomer = getCustomerFromList(userInputString, customerList);
+
+                    if (!currentMemberCheck(currentCustomer)) {
+                        System.out.println("Personen har inte ett aktivt medlemskap. \n" +
+                                currentCustomer.getName() + " blev medlem "
+                                + currentCustomer.getDateOfMembership());
+                    } else {
+                        System.out.println(currentCustomer.getName() + " med personnummer " +
+                                currentCustomer.getPersonalNumber() + " är aktiv medlem på Best Gym Ever. Välkommen!");
+                        printToPTInfo(currentCustomer, toPTFilePath);
+                    }
+                }
+                System.out.println("Vill du fortsätta? [Ja/Nej]");
+                if (!userScan.nextLine().trim().equalsIgnoreCase("ja")) {
+                    programLoop = false;
+                }
+            }
         } catch (FileNotFoundException e) {
             System.out.println("File not found.");
             e.printStackTrace();
@@ -121,6 +131,7 @@ public class TheGym {
         }
     }
 
+    //bryter den statiska kontexten
     public static void main(String[] args) {
         TheGym theGym = new TheGym();
         theGym.mainProgram();
--- test/CustomerTest.java
+++ test/CustomerTest.java
@@ -1,5 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CustomerTest {
+    Customer curre = new Customer("1234564321", "Curre Magén", "2019-08-18");
+
+    @Test
+    public void updateMembershipTest() {    //Testar att metoden ändrar medlemsdatum till dagens datum.
+        curre.updateMembership();
+
+        assertEquals(curre.getDateOfMembership(), LocalDate.now().toString());
+        assertNotEquals("2019-08-18", curre.getDateOfMembership());
+    }
 
 }
